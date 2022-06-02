bdb76a1fc0bf3ca6a66fd240f36668ddcc46c3e3
Davith Chhung <chhungdavith@gmail.com>
2022-06-02 14:44:53 +0200

    create connect and disconnect user function

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 // import Counter from "./components/Counter.vue";
 // import DecodeJwtForm from "./components/DecodeJwtForm.vue";
-import Tableau from "./components/Tableau.vue";
+// import Tableau from "./components/Tableau.vue";
+import Profile from "./components/Profile.vue";
 </script>
 
 <template>
   <!-- <Counter />
   <hr />
   <DecodeJwtForm /> -->
-  <Tableau />
+  <!-- <Tableau /> -->
+  <Profile />
 </template>
